d0025d16baba12ca4b5426c6a918f4bb75e17f33
Abarna <abarnaselvaganapathy@gmail.com>
2026-01-11 16:59:58 +0530

    Updated some changes and executed the project again

--- src/main/java/com/automation/framework/drivers/DriverFactory.java
+++ src/main/java/com/automation/framework/drivers/DriverFactory.java
@@ -8,39 +8,39 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
-    private static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>(); //ThreadLocal container
+	private static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>(); //ThreadLocal container
 
-    public static WebDriver initDriver(String browser) {
-        if (browser == null || browser.isEmpty()) {
-        	browser = ConfigReader.get("browser");
-        }
-        if (browser == null) {
-        	browser = "chrome";
-        }
-        if (browser.equalsIgnoreCase("chrome")) {
-            WebDriverManager.chromedriver().setup();
-            tlDriver.set(new ChromeDriver());
-        } else if (browser.equalsIgnoreCase("firefox")) {
-            WebDriverManager.firefoxdriver().setup();
-            tlDriver.set(new FirefoxDriver());
-        } else {
-            WebDriverManager.chromedriver().setup();
-            tlDriver.set(new ChromeDriver());
-        }
-        int implicit = ConfigReader.getInt("implicit.wait", 5);
-        getDriver().manage().window().maximize();
-        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(implicit));
-        return getDriver();
-    }
+	public static WebDriver initDriver(String browser) {
+		if (browser == null || browser.isEmpty()) {
+			browser = ConfigReader.get("browser");
+		}
+		if (browser == null) {
+			browser = "chrome";
+		}
+		if (browser.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			tlDriver.set(new ChromeDriver());
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			tlDriver.set(new FirefoxDriver());
+		} else {
+			WebDriverManager.chromedriver().setup();
+			tlDriver.set(new ChromeDriver());
+		}
+		int implicit = ConfigReader.getInt("implicit.wait", 5);
+		getDriver().manage().window().maximize();
+		getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(implicit));
+		return getDriver();
+	}
 
-    public static WebDriver getDriver() {
-        return tlDriver.get(); //Returns the actual WebDriver for current thread
-    }
+	public static WebDriver getDriver() {
+		return tlDriver.get(); //Returns the actual WebDriver for current thread
+	}
 
-    public static void quitDriver() {
-        if (tlDriver.get() != null) {
-            tlDriver.get().quit();
-            tlDriver.remove();
-        }
-    }
+	public static void quitDriver() {
+		if (tlDriver.get() != null) {
+			tlDriver.get().quit();
+			tlDriver.remove();
+		}
+	}
 }
